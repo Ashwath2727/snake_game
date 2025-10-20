2256f3d113c9888987a54cb3a25a11db168bc43d
Ashwath S <miniash123@outlook.com>
2025-10-20 21:21:54 +0530

    code for controlling snake

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ my_screen.tracer(0)
 
 snake = Snake()
 
+my_screen.listen()
+my_screen.onkey(snake.up, "Up")
+my_screen.onkey(snake.down, "Down")
+my_screen.onkey(snake.left, "Left")
+my_screen.onkey(snake.right, "Right")
+
 
 is_game_on = True
 while is_game_on:
