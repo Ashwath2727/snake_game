a38d22dbbcee132e8235d58409ea989ae8d51542
Ashwath S <miniash123@outlook.com>
2025-10-21 18:39:34 +0530

    added code to detect collision with tail

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ while is_game_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
 
@@ -39,6 +40,15 @@ while is_game_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         is_game_on = False
         scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+
+        elif snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
 
 
 my_screen.exitonclick()
--- snake.py
+++ snake.py
@@ -17,12 +17,7 @@ class Snake:
     def create_snake(self):
 
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
 
     def move(self):
@@ -49,3 +44,14 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
