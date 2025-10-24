4859c767ca8c1211e63fbd24428fdea0fbaf3303
Ashwath S <miniash123@outlook.com>
2025-10-24 21:33:14 +0530

    added logic for file writing

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,10 @@ class Scoreboard(Turtle):
         super().__init__()
 
         self.score = 0
-        self.high_score = 0
+
+        with open("..\data.txt", "r") as file:
+            self.high_score = int(file.read())
+
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -33,4 +36,8 @@ class Scoreboard(Turtle):
         if self.score > self.high_score:
             self.high_score = self.score
 
+            with open("..\data.txt", "w") as file:
+                file.write(str(self.high_score))
+
         self.score = 0
+        self.update_scoreboard()
